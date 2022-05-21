a4a3f3cf4487c807e0fc12779fc7588a47546fba
Victerst <78637179+Victerst@users.noreply.github.com>
2022-05-21 20:35:30 +0700

    add lab1

---
+++ lab1/HW1.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace lab1vs_pr
+{
+    public class HW1
+    {
+        public static long QueueTime(int[] customers, int n)
+        {
+            int[] time_customers = new int[n];
+            int k, time = 0, i;
+
+            for (k = 0; k < n; k++) 
+                time_customers[k] = customers[k];
+            
+            while (k < n || time_customers.Any(c => c > 0))
+            {
+                for (i = 0; i < n; i++)
+                {
+                    time_customers[i] -= 1;
+
+                    if (time_customers[i] == 0 && k < customers.Length)
+                        time_customers[i] = customers[k++];
+                }
+                time++;
+            }
+            return time;
+        }
+    }
+}
---
+++ lab1/Programm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lab1vs_pr
+{
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Тест №1: [5,3,4], 1: ");
+            Console.WriteLine(HW1.QueueTime(new int[] { 5, 3, 4 }, 1));
+
+            Console.WriteLine("\nТест №2: [10,2,3,3], 2: ");
+            Console.WriteLine(HW1.QueueTime(new int[] { 10, 2, 3, 3 }, 2));
+
+            Console.WriteLine("\nТест №3: [2,3,10], 2: ");
+            Console.WriteLine(HW1.QueueTime(new int[] { 2, 3, 10 }, 2));
+
+            /*Это так
+            Console.WriteLine("Сколько всего касс? ");
+            int kas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Сколько всего покупателей? ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nВведите время каждого человека, которое они проводят за кассой ");
+
+            int[] time_customers = new int[n];
+            for (int k = 0; k < n; k++)
+            {
+                time_customers[k] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine(HW1.QueueTime(time_customers, kas));
+            */
+        }
+    }
+}
